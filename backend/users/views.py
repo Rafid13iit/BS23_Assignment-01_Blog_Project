890404a59b9c311a6adabad08e6293bbf8d59a8c
Rafid13iit <113895411+Rafid13iit@users.noreply.github.com>
2025-03-20 19:13:09 +0600

    JWT primary authentication module done!

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import CustomUser
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
